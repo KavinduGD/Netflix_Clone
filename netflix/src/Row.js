400cb6fdaf788efab851876b9e6b8727ad29bbbd
it21038914 <it21038914@my.sliit.lk>
2023-06-23 01:53:17 +0530

    deployed in firebase

---
+++ netflix/src/Row.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+import axios from "./Axios";
+
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setmovies] = useState([]);
+  const baseURL = "https://image.tmdb.org/t/p/original/";
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setmovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => {
+          return (
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row_poster ${isLargeRow ? "row_posterLarger" : ""}`}
+                key={movie.id}
+                src={`${baseURL}${
+                  isLargeRow ? movie.poster_path : movie?.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
